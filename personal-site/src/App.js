778539c0702e5b02619b86451d0deb17aed74c87
GDiamante <gdiamante123@gmail.com>
2021-01-05 18:52:58 -0500

    Updates to Pages, Added menu bar

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React , { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import MenuBar from './Components/MenuBar';
+import BasePage from './Components/BasePage';
+
+const HOME_ROUTE = '/'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+      <Route component={MenuBar}/>
+        <Switch>
+          <Route exact path="/">
+            <BasePage title='Home'/>
+          </Route>
+          <Route exact path="/about">
+            <BasePage title='About'/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
 
 export default App;
