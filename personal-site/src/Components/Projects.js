96d57572a17794e63e6660b9f45190867664b9ed
GDiamante <gdiamante123@gmail.com>
2021-01-10 17:29:37 -0500

    Added projects, changed colors, updated styling

--- personal-site/src/Components/Projects.js
+++ personal-site/src/Components/Projects.js
@@ -1,16 +1,54 @@
 import React from 'react'
 import '../Styles/projects.scss';
+import { Button, Row, Col } from 'antd';
 
 export default function Projects(props) {
     return (
+    <>
         <div className='projects'>
-            <div className='projectsHeading'>
-                <h3>My Projects</h3>
+            <div className='projectsBox'>
+                <div className='projectsHeading'>
+                    <h3>Notify Me</h3>
+                </div>
+                <div className='projectsBody'>
+                    <Row>
+                        <p>IOS app to create push notifications with custom text
+                        </p>
+                    </Row>
+                    <Row className='projectInfo'>
+                        <Col>
+                            In Progress
+                        </Col>
+                        <Col>
+                            <Button href='https://github.com/GDiamante/Notify-Me' target='_blank'>
+                                Explore
+                            </Button>
+                        </Col>
+                    </Row>
+                </div>
             </div>
-            <div className='projectsBody'>
-                <p>My project list
-                </p>
+            <div className='projectsBox'>
+                <div className='projectsHeading'>
+                    <h3>This Website</h3>
+                </div>
+                <div className='projectsBody'>
+                    <Row>
+                        <p>React app created for fun outlining my portfolio
+                        </p>
+                    </Row>
+                    <Row className='projectInfo'>
+                        <Col>
+                            In Progress
+                        </Col>
+                        <Col>
+                            <Button href='https://github.com/GDiamante/gdiamante.github.io' target='_blank'>
+                                Explore
+                            </Button>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         </div>
+    </>
     )
 }
