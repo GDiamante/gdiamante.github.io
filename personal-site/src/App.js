8611c36186b1d85a7619d5ef5938ba632d724053
GDiamante <gdiamante123@gmail.com>
2021-01-11 22:34:00 -0500

    Add picture, shift menu height

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,10 +1,11 @@
 import './Styles/App.css';
-import React , { useState, useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+
 
 import Menu from './Components/Menu';
 import About from './Components/About';
@@ -16,27 +17,30 @@ const ABOUT_ROUTE = '/about'
 const PROJECTS_ROUTE = '/projects'
 const RESUME_ROUTE = '/resume'
 
-function App(props) {
-  document.body.style = 'background: #303030; height: inherit;';
-  return (
-    <Router>
-      <div className="App">
-      <Route component={Menu}/>
-        <Switch>
-          <Route exact path={HOME_ROUTE} />
-          <Route exact path={ABOUT_ROUTE}>
-            <About />
-          </Route>
-          <Route exact path={PROJECTS_ROUTE}>
-            <Projects />
-          </Route>
-          <Route exact path={RESUME_ROUTE}>
-            <Resume />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+
+  render() {
+    document.body.style = 'background: #303030; height: inherit;';
+    return (
+      <Router>
+        <div className="App">
+        <Route component={Menu}/>
+          <Switch>
+            <Route exact path={HOME_ROUTE} />
+            <Route exact path={ABOUT_ROUTE}>
+              <About />
+            </Route>
+            <Route exact path={PROJECTS_ROUTE}>
+              <Projects />
+            </Route>
+            <Route exact path={RESUME_ROUTE}>
+              <Resume />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
